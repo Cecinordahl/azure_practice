9bb877a630bae40ab29389a8cb9e12af330aef45
cecinordahl <cecilie.nordahl@me.com>
2021-11-12 13:46:01 +0100

    pom og datasource

--- src/main/java/com/example/azure_practice/AzurePracticeApplication.java
+++ src/main/java/com/example/azure_practice/AzurePracticeApplication.java
@@ -1,5 +1,6 @@
 package com.example.azure_practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +8,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @RestController
 public class AzurePracticeApplication {
+
+    @Autowired
+    DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(AzurePracticeApplication.class, args);
@@ -25,5 +30,10 @@ public class AzurePracticeApplication {
     @GetMapping("/user-agent")
     public String userAgent(HttpServletRequest request) {
         return request.getHeader("User-Agent");
+    }
+
+    @GetMapping("/")
+    public String databaseTest(){
+        return "SELECT 1";
     }
 }
